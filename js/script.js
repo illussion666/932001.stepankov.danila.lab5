744e840e3f1b7b5756c2703add17b5693cca87ea
illussion666 <115892979+illussion666@users.noreply.github.com>
2023-01-07 06:54:13 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+butt.onclick = function () {
+    l1.style.display = "block"
+    td1.style.display = "block"
+}
+butt2.onclick = function () {
+    l2.style.display = "block"
+    td2.style.display = "block"
+}
+butt3.onclick = function () {
+    l3.style.display = "block"
+    td3.style.display = "block"
+}
+
+
+l1.onclick = function () {
+    l1.style.display = "none"
+    td1.style.display = "none"
+}
+l2.onclick = function () {
+    l2.style.display = "none"
+    td2.style.display = "none"
+}
+l3.onclick = function () {
+    l3.style.display = "none"
+    td3.style.display = "none"
+}
+
+
+
+news.onmouseover = function (){
+    news.style.background = "red";
+};
+
+news.onmouseleave = function () {
+    news.style.background = "aqua";
+};
+
+news2.onmouseover = function () {
+    news2.style.background = "red";
+};
+
+news2.onmouseleave = function () {
+    news2.style.background = "aqua";
+};
+
+news3.onmouseover = function (){
+    news3.style.background = "red";
+};
+
+news3.onmouseleave = function () {
+    news3.style.background = "aqua";
+};
